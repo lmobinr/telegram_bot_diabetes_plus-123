ad98f7e3b89923c5d0ef16c78d7622fc9209f83d
lmobinr <mr.1gamerpubg1@gmail.com>
2025-10-02 19:54:43 +0330

    Update bot_telegram.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -51,6 +51,7 @@ def home():
 if __name__ == "__main__":
     # ست کردن وبهوک    
     application.bot.set_webhook(url='https://telegram-bot-diabetes-plus-123.onrender.com' + "/webhook")
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int("PORT", 5000))
+
 
 
